2bc9153b1ce2780c63969d336f293c3338545205
Ela Semra Sava <elassavaa@gmail.com>
2024-11-15 00:06:06 +0300

    audio question gosterimi ve db kaydetme tamamlandi

--- lib/screens/audio_screens.dart
+++ lib/screens/audio_screens.dart
@@ -15,112 +15,157 @@ class AudioScreen extends StatefulWidget {
 class _AudioScreenState extends State<AudioScreen> {
   late AudioPlayer _audioPlayer;
   String? _audioUrl;
-  String? _correctOption;
-  List<String> _options = [];
-  bool _isPlaying = false;
+  int? _correctOption; // _correctOption as int (1-6 range)
+  List<String> _options = ['Mutlu', 'Üzgün', 'Kızgın', 'Korkmuş', 'Şaşırmış', 'İğrenmiş'];
   bool _isLoading = true;
   bool _isAnswered = false;
-
-  Duration _currentPosition = Duration.zero;
-  Duration _totalDuration = Duration.zero;
+  int _questionNumber = 0; // Track question number
+  String _questionId = ''; // Track question ID
+  List<DocumentSnapshot> _allQuestions = []; // List of all questions
+  List<DocumentSnapshot> _remainingQuestions = []; // Remaining questions to show
 
   @override
   void initState() {
     super.initState();
     _audioPlayer = AudioPlayer();
-    _fetchAudioQuestion();
-
-    // Listen to playback completion and reset state
-    _audioPlayer.onPlayerStateChanged.listen((state) {
-      if (state == PlayerState.completed) {
-        setState(() {
-          _isPlaying = false;
-        });
-      }
-    });
-
-    // Listen to the current position of the audio
-    _audioPlayer.onPositionChanged.listen((position) {
-      setState(() {
-        _currentPosition = position;
-      });
-    });
-
-    // Listen to the total duration of the audio
-    _audioPlayer.onDurationChanged.listen((duration) {
-      setState(() {
-        _totalDuration = duration;
-      });
-    });
+    _fetchAllQuestions(); // Fetch all 20 questions at the start
   }
 
-  // Fetch a random audio question from Firestore
-  Future<void> _fetchAudioQuestion() async {
+  // Fetch all audio questions from Firestore
+  Future<void> _fetchAllQuestions() async {
     setState(() {
       _isLoading = true;
       _isAnswered = false;
+      _questionNumber = 0;
     });
 
     try {
       QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('audioQuestions').get();
 
       if (querySnapshot.docs.isNotEmpty) {
-        DocumentSnapshot audioQuestionSnapshot = (querySnapshot.docs..shuffle()).first;
-
         setState(() {
-          _audioUrl = audioQuestionSnapshot['audioUrl'];
-          _correctOption = audioQuestionSnapshot['correctOption'];
-          _options = List<String>.from(audioQuestionSnapshot['options']);
+          _allQuestions = querySnapshot.docs;
+          _remainingQuestions = List.from(_allQuestions); // Copy all questions to remaining list
           _isLoading = false;
         });
-        print('Random question loaded with audio URL: $_audioUrl');
+
+        _fetchAudioQuestion(); // Load the first question
       } else {
         print('No audio questions available in the collection');
         throw Exception('No audio questions found in the collection');
       }
     } catch (e) {
-      print('Error fetching audio question: $e');
+      print('Error fetching audio questions: $e');
       setState(() {
         _isLoading = false;
       });
     }
   }
 
-  // Play or pause the audio
-  void _playPauseAudio() {
-    if (_audioUrl != null) {
-      if (_isPlaying) {
-        _audioPlayer.pause();
-      } else {
-        _audioPlayer.play(UrlSource(_audioUrl!));
-      }
+  // Fetch a random audio question from the remaining list
+Future<void> _fetchAudioQuestion() async {
+  if (_remainingQuestions.isEmpty) {
+    // If no remaining questions, show "Test Bitti"
+    _showTestFinishedDialog();
+    return;
+  }
 
-      setState(() {
-        _isPlaying = !_isPlaying;
-      });
+  setState(() {
+    _isLoading = true;
+    _isAnswered = false;
+    _questionNumber++;
+  });
+
+  // Get a random question from remaining questions
+  DocumentSnapshot audioQuestionSnapshot = _remainingQuestions.removeAt(0);
+
+  print('Loading question ${_questionNumber}: Question ID - ${audioQuestionSnapshot.id}');
+
+  // Check if the question has all necessary fields
+  try {
+    setState(() {
+      _audioUrl = audioQuestionSnapshot['audioUrl'];
+      _correctOption = audioQuestionSnapshot['correctOption'] as int;
+      _questionId = audioQuestionSnapshot.id; // Get the question ID
+      _isLoading = false;
+    });
+
+    // Ensure that the question URL is valid
+    if (_audioUrl != null) {
+      print('Audio URL for Question ${_questionNumber}: $_audioUrl');
+    } else {
+      print('Error: Audio URL is null for question ID ${audioQuestionSnapshot.id}');
+    }
+
+    // Play audio automatically on question load
+    _playAudio();
+  } catch (e) {
+    print('Error fetching audio question: $e');
+    setState(() {
+      _isLoading = false;
+    });
+  }
+}
+
+
+  // Play audio
+  void _playAudio() {
+    if (_audioUrl != null) {
+      _audioPlayer.play(UrlSource(_audioUrl!));
     } else {
       print('Audio URL is null');
     }
   }
 
-  // Submit the selected emotion
-  void _submitEmotion(String selectedEmotion) {
+  // Save answer to Firebase
+  Future<void> _saveAnswer(int selectedEmotionIndex) async {
     if (_correctOption != null) {
-      bool isCorrect = selectedEmotion == _correctOption;
+      bool isCorrect = selectedEmotionIndex + 1 == _correctOption;
+      // Create the answer data
+      Map<String, dynamic> answerData = {
+        'audioUrl': _audioUrl,
+        'correctOption': _correctOption,
+        'isCorrect': isCorrect,
+        'questionNumber': _questionNumber,
+        'selectedOption': _options[selectedEmotionIndex],
+        'timestamp': FieldValue.serverTimestamp(), // Save current time
+      };
+
+      try {
+        await FirebaseFirestore.instance
+            .collection('patients')
+            .doc(widget.patientId)
+            .collection('audioQuestions') // Or 'videoQuestions' / 'maskedQuestions'
+            .doc(_questionId) // Question ID
+            .set({
+          _questionNumber.toString(): answerData, // Save based on question number
+        }, SetOptions(merge: true)); // Merge to avoid overwriting other answers
+        print('Answer saved successfully');
+      } catch (e) {
+        print('Error saving answer: $e');
+      }
+    }
+  }
+
+  // Submit the selected emotion
+  void _submitEmotion(int selectedEmotionIndex) {
+    if (_correctOption != null) {
+      bool isCorrect = selectedEmotionIndex + 1 == _correctOption;
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
           title: Text(isCorrect ? 'Correct!' : 'Incorrect'),
           content: Text(
             isCorrect
-                ? 'You selected the correct emotion: $selectedEmotion'
-                : 'The correct emotion was: $_correctOption',
+                ? 'You selected the correct emotion: ${_options[selectedEmotionIndex]}'
+                : 'The correct emotion was: ${_options[_correctOption! - 1]}', // Adjust for 1-6 range
           ),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.pop(context);
-                _goToNextQuestion();
+                _saveAnswer(selectedEmotionIndex); // Save the answer
+                _goToNextQuestion(); // Go to next question
               },
               child: Text('Next Question'),
             ),
@@ -134,9 +179,28 @@ class _AudioScreenState extends State<AudioScreen> {
   void _goToNextQuestion() {
     setState(() {
       _audioPlayer.stop();
-      _isPlaying = false;
-      _fetchAudioQuestion();
+      _fetchAudioQuestion(); // Load the next question
     });
+  }
+
+  // Show the "Test Finished" dialog
+  void _showTestFinishedDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Test Bitti'),
+        content: Text('Tebrikler, testi tamamladınız!'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.pop(context); // Go back to the previous screen
+            },
+            child: Text('Tamam'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -160,32 +224,6 @@ class _AudioScreenState extends State<AudioScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed: _playPauseAudio,
-                    child: Text(_isPlaying ? 'Pause' : 'Play'),
-                  ),
-                  SizedBox(height: 16),
-                  // Display audio progress bar
-                  if (_totalDuration > Duration.zero)
-                    Column(
-                      children: [
-                        Slider(
-                          min: 0,
-                          max: _totalDuration.inSeconds.toDouble(),
-                          value: _currentPosition.inSeconds.toDouble(),
-                          onChanged: (value) async {
-                            final position = Duration(seconds: value.toInt());
-                            await _audioPlayer.seek(position);
-                          },
-                        ),
-                        Text(
-                          '${_currentPosition.inMinutes}:${(_currentPosition.inSeconds % 60).toString().padLeft(2, '0')} / ${_totalDuration.inMinutes}:${(_totalDuration.inSeconds % 60).toString().padLeft(2, '0')}',
-                          style: GoogleFonts.poppins(fontSize: 16),
-                        ),
-                      ],
-                    ),
-                  SizedBox(height: 32),
-                  // Emotion selection options
                   Text(
                     'How do you feel after listening?',
                     style: GoogleFonts.poppins(fontSize: 18),
@@ -194,28 +232,19 @@ class _AudioScreenState extends State<AudioScreen> {
                   Wrap(
                     spacing: 10.0,
                     runSpacing: 10.0,
-                    children: _options.map((emotion) {
+                    children: List.generate(_options.length, (index) {
                       return ElevatedButton(
                         onPressed: _isAnswered
                             ? null
                             : () {
                                 setState(() {
-                                  _isAnswered = true;
+                                  _isAnswered = true; // Prevent multiple answers
                                 });
-                                _submitEmotion(emotion);
+                                _submitEmotion(index); // Submit the selected emotion
                               },
-                        child: Text(emotion, style: GoogleFonts.poppins(fontSize: 16)),
+                        child: Text(_options[index], style: GoogleFonts.poppins(fontSize: 16)),
                       );
-                    }).toList(),
-                  ),
-                  SizedBox(height: 32),
-                  // Next audio button
-                  ElevatedButton(
-                    onPressed: _goToNextQuestion,
-                    child: Text('Next Audio'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                    ),
+                    }),
                   ),
                 ],
               ),
